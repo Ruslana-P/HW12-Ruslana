7033c273941913d58f31f7b1d2f45df35401d82a
Ruslana Pavliuk <pavliuk_r_v@protonmail.com>
2022-05-10 09:07:34 +0300

    home work 12 with extra console.log2

---
+++ code.js
@@ -0,0 +1,66 @@
+const small_size = {
+    price: 50,
+    calories: 20}
+;
+
+const medium_size = {
+    price: 75,
+    calories: 30
+}
+
+const big_size = {
+    price: 100,
+    calories: 40
+    }
+
+const extraCheese ={
+    price: 10,
+    calories: 20
+}
+
+const extraSalad = {
+    price: 20,
+    calories: 5
+}
+
+const potato = {
+    price: 15,
+    calories: 10
+}
+
+const extraSpicy = {
+    price: 15,
+    calories: 0
+}
+
+const topping_mayo = {
+    price: 20,
+    calories: 5
+}
+
+function Hamburger (size) {
+    this.price = size.price;
+    this.calories = size.calories;
+    this.getPrice = function() {
+        return this.price;
+    }
+    this.getCalories =  function() {
+        return this.calories;
+    }
+    this.addModifier = function(modifier){
+        this.price = this.price + modifier.price;
+        this.calories = this.calories + modifier.calories;
+    }
+}
+
+const hamburger = new Hamburger(small_size);
+console.log(hamburger);
+
+hamburger.addModifier(topping_mayo);
+console.log(hamburger);
+
+hamburger.addModifier(potato);
+console.log(hamburger);
+
+console.log('Price with sauce: ' + hamburger.getPrice());
+console.log('Calories with sauce: ' + hamburger.getCalories());
