e9f8069dab5826d81935e21d7065091b3ef5cc54
Ruslana Pavliuk <pavliuk_r_v@protonmail.com>
2022-05-15 10:34:14 +0300

    home work12 after refinement

--- code.js
+++ code.js
@@ -1,55 +1,69 @@
 const small_size = {
+    name: 'small_burger',
     price: 50,
-    calories: 20}
-;
+    calories: 20
+}
 
 const medium_size = {
+    name: 'medium_burger',
     price: 75,
     calories: 30
 }
 
 const big_size = {
+    name: 'big_burger',
     price: 100,
     calories: 40
     }
 
-const extraCheese ={
+const extraCheese = {
+    name: 'extraCheese',
     price: 10,
     calories: 20
 }
 
 const extraSalad = {
+    name: 'extraSalad',
     price: 20,
     calories: 5
 }
 
 const potato = {
+    name: 'potato',
     price: 15,
     calories: 10
 }
 
 const extraSpicy = {
+    name: 'extraSpicy',
     price: 15,
     calories: 0
 }
 
 const topping_mayo = {
+    name: 'topping_mayo',
     price: 20,
     calories: 5
 }
 
-function Hamburger (size) {
+function Hamburger(size) {
+    this.name = size.name;
     this.price = size.price;
     this.calories = size.calories;
+    this.modifiers = [];
+
     this.getPrice = function() {
         return this.price;
     }
-    this.getCalories =  function() {
+
+    this.getCalories = function() {
         return this.calories;
     }
-    this.addModifier = function(modifier){
+
+    this.addModifier = function(modifier) {
         this.price = this.price + modifier.price;
         this.calories = this.calories + modifier.calories;
+        this.modifiers.push(modifier);
     }
 }
 
@@ -60,3 +74,4 @@ hamburger.addModifier(potato);
 
 console.log('Price with sauce: ' + hamburger.getPrice());
 console.log('Calories with sauce: ' + hamburger.getCalories());
+console.log(hamburger.modifiers);
